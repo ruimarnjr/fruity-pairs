593c03221bdcb701d013a85a381e3b8b08f0b9fb
Ruimar Junior <ruimarnjr@gmail.com>
2023-07-01 18:17:56 +0000

    Update final step on the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,38 +11,30 @@ function flipCard() {
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
+    // first click
     hasFlippedCard = true;
     firstCard = this;
-
     return;
   }
 
+  // second click
   secondCard = this;
+
   checkForMatch();
 }
 
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
-  isMatch ? disableCards() : unflipCards();
-}
-
-function disableCards() {
-  firstCard.removeEventListener('click', flipCard);
-  secondCard.removeEventListener('click', flipCard);
-
-  resetBoard();
-}
-
-function unflipCards() {
-  lockBoard = true;
-
-  setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
-
-    resetBoard();
-  }, 1500);
+  if (isMatch) {
+    disableCards();
+  } else {
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+      resetBoard();
+    }, 1500);
+  }
 }
 
 function resetBoard() {
